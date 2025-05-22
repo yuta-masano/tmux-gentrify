f8b5aabf7f4b9dc4139d3cd4f091374d79c63f0c
Chris McKenzie <kristopolous@yahoo.com>
2025-05-22 14:04:31 -0700

    stack based

--- scripts/cut.sh
+++ scripts/cut.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 pane_id=$(tmux display-message -p "#{pane_id}")
-tmux set-option -gq @cut_pane "$pane_id"
+stack=$(tmux show -gqv @gentrify_pane_stack)
+if [[ -z "$stack" ]]; then
+  new_stack="$pane_id"
+else
+  new_stack="$pane_id,$stack"
+fi
 tmux select-pane -t "$pane_id" -P "bg=yellow"
+tmux set -g @gentrify_pane_stack "$new_stack"
 tmux break-pane -d -s "$pane_id"
--- scripts/paste.sh
+++ scripts/paste.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-pane_id=$(tmux show-option -gqv @cut_pane)
-if [ -z "$pane_id" ]; then
-  tmux display-message "No pane in cut buffer"
+stack=$(tmux show -gqv @gentrify_pane_stack)
+if [[ -z "$stack" ]]; then
+  tmux display-message "Gentrify: paste buffer empty"
   exit 1
 fi
 
+# Extract first pane_id from stack
+IFS=',' read -r first_pane rest <<< "$stack"
+
+# Update stack without first pane
+tmux set -g @gentrify_pane_stack "$rest"
+
+
 # Try joining pane to original window first
-if ! tmux join-pane -s "$pane_id"; then
+if ! tmux join-pane -s "$first_pane"; then
   # If that fails, join to current window instead
   current_window=$(tmux display-message -p "#{window_id}")
-  tmux join-pane -s "$pane_id" -t "$current_window"
+  tmux join-pane -s "$fist_pane" -t "$current_window"
 fi
 
-tmux select-pane -t "$pane_id" -P ""
-tmux set-option -gq @cut_pane ""
+tmux select-pane -t "$first_pane" -P ""
+
